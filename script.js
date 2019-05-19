0414ca89d0191112a4606379e6e025d6d46ecef4
VladimirTi <tytmes@mail.ru>
2019-05-19 23:56:50 +0300

    Solution

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector('th').addEventListener('click', sortingByAge)
+
+function sortingByAge() {
+  const rows = [...document.querySelector('tbody').rows];
+  rows.sort((a, b) => {
+    return a.firstElementChild.innerText - b.firstElementChild.innerText;
+  })
+
+  rows.forEach((node) => {
+    document.querySelector('table > tbody').append(node)
+  })
+}
