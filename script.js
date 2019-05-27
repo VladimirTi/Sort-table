801c854c3a49fcdfb8d55f1561b9153943c206eb
VladimirTi <tytmes@mail.ru>
2019-05-27 20:14:47 +0300

    correction

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+const rows = [...document.querySelector('tbody').rows];
+
 document.querySelector('thead').addEventListener('click', (event) => {
   const index = +event.target.dataset.index;
-  const rows = [...document.querySelector('tbody').rows];
 
-  rows.sort((a, b) => {
-    [a, b] = [a.children[index].innerText, b.children[index].innerText]
+  rows.sort((elem1, elem2) => {
+    [a, b] = [elem1.children[index].innerText, elem2.children[index].innerText]
     if (!isNaN(+a)) return a - b;
     return a > b ? 1 : -1;
   })
+
   const container = document.createElement('tbody');
+
   rows.forEach((node) => {
     container.append(node);
   })
+
   document.querySelector('tbody').replaceWith(container);
 });
